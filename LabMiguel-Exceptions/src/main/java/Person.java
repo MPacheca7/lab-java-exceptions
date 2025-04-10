50afc815fa898e0c078ab54ca79904aa12dbbc87
Pacheca <mpacheca7@gmail.com>
2025-04-10 18:04:04 +0200

    iniciando el lab

---
+++ LabMiguel-Exceptions/src/main/java/Person.java
@@ -0,0 +1,48 @@
+public class Person {
+    private int id;
+    private String name;
+    private int age;
+    private String ocupation;
+
+    public Person(int id, String name, int age, String ocupation) {
+        this.id = id;
+        this.name = name;
+        setAge(age);
+        this.ocupation = ocupation;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age < 0) {
+            throw new IllegalArgumentException("Introduce un valor mayor que 0");
+        } // genera un error si age es menor que 0.
+        this.age = age;
+    }
+
+    public String getOcupation() {
+        return ocupation;
+    }
+
+    public void setOcupation(String ocupation) {
+        this.ocupation = ocupation;
+    }
+}
---
+++ LabMiguel-Exceptions/src/main/java/PersonsList.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class PersonsList {
+    private ArrayList<Person> persons;
+
+    public PersonsList(ArrayList<Person> persons) {
+        this.persons = persons;
+    }
+
+    public ArrayList<Person> getPersons() {
+        return persons;
+    }
+
+    public void setPersons(ArrayList<Person> persons) {
+        this.persons = persons;
+    }
+
+    private String findByName(String name) {
+
+    }
+}
